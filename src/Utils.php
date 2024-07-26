837a42c6341f8e9ebe9637e0aa49a81e8a01d78e
Ember <evebay@mail.ru>
2024-07-27 00:36:07 +0700

    fix lint2

--- src/Utils.php
+++ src/Utils.php
@@ -3,12 +3,7 @@
 namespace Php\Package;
 
 // Эта функция переворачивает переданную строку
-function reverseString($string)
-{
-    return implode(array_reverse(str_split($string)));
-}
-
-function reverseString2($string)
+function reverseString($string): string
 {
     return implode(array_reverse(str_split($string)));
 }
